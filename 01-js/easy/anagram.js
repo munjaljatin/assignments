d83f6187452a68e1e5b53749b834941fd95874d6
Jatin Munjal <munjal.jatin24@gmail.com>
2024-01-10 21:00:31 +0530

    solution-anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,20 @@
 */
 
 function isAnagram(str1, str2) {
+  let length1 = str1.length;
+  let length2 = str2.length;
 
+  if (length1 !== length2) return false;
+  str1 = str1.replace(/\s/g, "").toLowerCase();
+  str2 = str2.replace(/\s/g, "").toLowerCase();
+
+  str1 = str1.split("").sort().join("");
+  str2 = str2.split("").sort().join("");
+
+  for (let i = 0; i < length1; i++) {
+    if (str1[i] != str2[i]) return false;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
